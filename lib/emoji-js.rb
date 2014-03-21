4c50591426cbe48cbc70a871d1cc0ad02dc60424
Jesse Herrick <jessegrantherrick@gmail.com>
2014-03-20 20:20:17 -0400

    Coffeescript building script complete.

--- lib/emoji-js.rb
+++ lib/emoji-js.rb
@@ -3,22 +3,59 @@ require "fileutils"
 require "uglifier"
 
 module EmojiJS
-  class Build
-    def initialize(image_path)
+  class Generate
+    def initialize(image_path = "/graphics/")
       @image_path = image_path
+      @path = File.dirname File.expand_path(__FILE__)
+      @vendor_path = "#{@path}/vendor"
     end
 
     public
     # takes CoffeeScript from 'vendor/coffee/emoji.coffee' and saves the corresponding JS to a variable
     def coffeescript
-      puts vendor_dir
-      
+      @coffee_path = "#{@vendor_path}/coffee"
+
+      # get image path
+      puts 'Emoji image path: (default: "/graphics/") '
+      image_path = gets.chomp
+      image_path = "/graphics/" if image_path.empty?
+
+      # coffee_grounds = modified coffescript
+      @coffee_grounds = replace_in_file("#{@coffee_path/emoji.coffee}", %q{Emoji.image_path = "/graphics/" # customize to your liking}, "Emoji.image_path = \"#{image_path}\"")
+
+      @generated_js = CoffeeScript.compile(coffee_grounds)
+      @ugly_js = uglify @generated_js
+
+      # get js dir
+      puts %q{Your project's JavaScript directory: (default: "js/") }
+      js_dir = gets.chomp
+      js_dir = "js" if js_dir.empty?
+
+      # write emoji.min.js
+      FileUtils.cd(js_dir)
+      write_to_file "emoji.min.js", @uglify
+      FileUtils.cd("../")
     end
 
+    # takes raw JS and uglifies it
+    def uglify(js)
+      Uglifier.compile(js)
+    end
+
+    
+
     private
-    def vendor_dir
-      vendor_dir = File.dirname(File.expand_path(__FILE__, "../vendor"))
-      raise "Vendor directory doesn't exist" unless File.directory? vendor_dir
+    # replace something for something else in a string
+    def replace_in_file(file, search, replace)
+      string = File.read(file)
+      string.sub(search, replace)
+    end
+
+    # write content to a file
+    def write_to_file(filename, content)
+      File.open(filename, "w") do |file|
+        file.write(content)
+      end
     end
   end
 end
